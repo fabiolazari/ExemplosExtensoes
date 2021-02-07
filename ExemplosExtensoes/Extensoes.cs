923168bfcf6891671dbf26d51fdb14027917ada8
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-02-07 19:56:23 -0300

    AtualizandoProjeto.

---
+++ ExemplosExtensoes/Extensoes.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ExemplosExtensoes
+{
+	public static class Extensoes
+	{
+		public static string ToMeuUpper(this string lstring)
+		{
+			return lstring.ToUpper();
+		}
+
+		public static string ToMeuLower(this string lstring)
+		{
+			return lstring.ToLower();
+		}
+
+		public static int ContaCaracter(this string lstring)
+		{
+			return lstring.Length;
+		}
+
+		public static string RepeteTexto(this string lstring, int quantidade)
+		{
+			string retorno = string.Empty;
+			for (int i = 1; i <= quantidade; i++)
+			{
+				retorno += lstring;
+			}
+			return retorno;
+		}
+
+		public static int Doblo(this Int32 numero)
+		{
+			return numero * 2;
+		}
+
+		public static int SomaCom(this Int32 numero, Int32 numero2)
+		{
+			return numero + numero2;
+		}
+
+		public static int SubtraiDe(this Int32 numero, Int32 numero2)
+		{
+			return numero2 - numero;
+		}
+
+		public static string ToDescription(this Enum Enum)
+		{
+			FieldInfo fi = Enum.GetType().GetField(Enum.ToString());
+
+			DescriptionAttribute[] attributes =
+				(DescriptionAttribute[])fi.GetCustomAttributes(
+				typeof(DescriptionAttribute),
+				false);
+
+			if (attributes != null &&
+				attributes.Length > 0)
+				return attributes[0].Description;
+			else
+				return Enum.ToString();
+		}
+
+		public static T GetValueFromDescription<T>(this string description)
+		{
+			var type = typeof(T);
+			if (!type.IsEnum)
+				throw new ArgumentException();
+
+			FieldInfo[] fields = type.GetFields();
+			var field = fields.SelectMany(f => f
+							  .GetCustomAttributes(typeof(DescriptionAttribute), false), (f, a) => new { Field = f, Att = a })
+							  .Where(a => ((DescriptionAttribute)a.Att).Description == description)
+							  .SingleOrDefault();
+
+			//return field == null ? "" : field.Field.Name.ToString();
+			return field == null ? default : (T)field.Field.GetRawConstantValue();
+		}
+
+	}
+}
---
+++ ExemplosExtensoes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ExemplosExtensoes
+{
+	public class Program
+	{
+		private enum eMesesAno
+		{
+			[Description("Janeiro")]
+			JAN,
+			[Description("Fevereiro")]
+			FEV,
+			[Description("Março")]
+			MAR,
+			[Description("Abril")]
+			ABR,
+			[Description("Maio")]
+			MAI,
+			[Description("Junho")]
+			JUN,
+			[Description("Julho")]
+			JUL,
+			[Description("Agosto")]
+			AGO,
+			[Description("Setembro")]
+			SET,
+			[Description("Outubro")]
+			OUT,
+			[Description("Novembro")]
+			NOV,
+			[Description("Dezembro")]
+			DEZ
+		}
+
+		static void Main(string[] args)
+		{
+			//strings
+			string nome = "Teste de String!";
+			string nomao = nome.ToMeuUpper();
+			string nominho = nome.ToMeuLower();
+			int quantidade = nome.ContaCaracter();
+			string nomeRepetido = nome.RepeteTexto(4);
+			int quantidadeTotal = nomeRepetido.ContaCaracter();
+
+			//Inteiros
+			int num = 14;
+
+			int doblo = num.Doblo();
+			int soma = num.SomaCom(23);
+			int sub = num.SubtraiDe(16);
+
+			//Enums
+			string mes = eMesesAno.NOV.ToDescription();
+			var descricao = mes.GetValueFromDescription<eMesesAno>();
+
+			Console.ReadLine();
+		}
+	}
+}
